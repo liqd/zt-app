e3102e818f5d775a76de3451f47ba1cfea9f130c
Kha <k.tran@liqd.net>
2021-05-18 13:52:29 +0100

    styles/constants: adding constants/libs to re-use
    Added FONTS, COLORS and SPACINGS constant libraries.

---
+++ theme/colors.js
@@ -0,0 +1,28 @@
+export const COLORS = {
+  // brand
+  primary: '#2d40cc',
+  secondary: '#90efe4',
+  tertiary: '#008dc5',
+
+  // priority
+  success: '#a3ef90',
+  info: this.primary,
+  warning: '#ffc107',
+  danger: '#a72b1e',
+
+  // utility
+  paper: {
+    main: '#fff',
+    inverted: this.text.main
+  },
+  text: {
+    main: '#1a1a1a',
+    inverted: this.paper.main
+  },
+  grey: {
+    extralight: '#d8d8d8',
+    light: '#4d4d4d',
+    medium: '#666',
+    dark: '#464a4c',
+  },
+};
