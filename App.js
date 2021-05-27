21eef3a8e8490a5d0f5c6f0fef4e28eb73d2ace0
Julian Dehm <j.dehm@liqd.net>
2021-05-27 13:54:46 +0200

    use sentry for automated error reporting

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import * as Sentry from 'sentry-expo'
 import IdeaNavigator from './navigation/IdeaNavigator';
+
+Sentry.init({
+  url: 'https://sentry.liqd.net',
+  dsn: '',
+  enableInExpoDevelopment: false,
+  debug: false, 
+});
 
 const App = () => {
   return (
