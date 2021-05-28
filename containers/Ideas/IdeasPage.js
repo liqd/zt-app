fdce106a06cc5806ee470880960df3ddc940bb48
Rine <k.schmid@liqd.net>
2021-05-28 16:18:20 +0200

    Ideas/IdeasPage: use central API to fetch ideas

--- containers/Ideas/IdeasPage.js
+++ containers/Ideas/IdeasPage.js
@@ -2,19 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { styles } from './IdeasPage.styles';
 import { IdeasList } from './IdeasList';
+import API from '../../BaseApi'
 
 export const IdeasPage = (props) => {
-  // Data fetching happens currently on highest level (App.js).
-  // Depending on how we manage state (redux/context), this needs
-  // to be shifted somewhere else.
   const [fetchedData, setFetchedData] = useState([]);
+  const moduleId = 335
 
   useEffect(() => {
-    fetch('https://aplus-dev.liqd.net/api/modules/335/ideas/')
-      .then((response) => response.json())
-      .then((unpackedData) => {
-        setFetchedData(unpackedData);
-      });
+      API.getIdeas(moduleId).then(response => setFetchedData(response))
   }, []);
 
   return (
