06c742768912997121e43c4d9ca99c00ac68e764
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-05-31 10:47:36 +0200

    ideas: add navigation to idea form

--- navigation/IdeaNavigator.js
+++ navigation/IdeaNavigator.js
@@ -2,11 +2,13 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
 import { Idea } from '../containers/Ideas/Idea';
+import { IdeaCreate } from '../containers/Ideas/IdeaCreate';
 import { IdeasPage } from '../containers/Ideas/IdeasPage';
 
 const IdeaNavigator = createStackNavigator({
   IdeasPage: IdeasPage,
-  IdeaDetail: Idea
+  IdeaDetail: Idea,
+  IdeaCreate: IdeaCreate
 });
 
 export default createAppContainer(IdeaNavigator);
