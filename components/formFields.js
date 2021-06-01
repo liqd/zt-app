c978528caa31533a8e360686c4791fe1d03318e8
phillimorland <phillippamorland@gmail.com>
2021-06-01 15:31:34 +0200

    adding new form field files

---
+++ components/formFields.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button, TextInput, View, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import CheckBox from '@react-native-community/checkbox';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+import { styles } from './formFields.styles';
+
+export const DropdownFormField = (props) => {
+  return (
+    <Picker
+      selectedValue={props.selectedValue}
+      onValueChange={(itemValue, itemIndex) =>
+        props.setSelectedLanguage(itemValue)
+      }>
+      <Picker.Item label="Java" value="java" />
+      <Picker.Item label="JavaScript" value="js" />
+    </Picker>
+  );
+};
+
+export const CheckBoxFormField = (props) => {
+  return (
+    <View>
+      <View>
+        <CheckBox
+          type={'checkbox'}
+          {...props}
+        />
+        <Text>{children}</Text>
+      </View>
+    </View>
+  );
+};
+
+export const TextInputFormField = (error, props) => {
+  return (
+    <View>
+      <Text style={styles.formLabel}>{props.field}</Text>
+      <TextInput
+        style={styles.textInput}
+        {...props}
+      />
+    </View>
+  );
+};
