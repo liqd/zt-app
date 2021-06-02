4e058cb26e0347ac2eed8d65e8c7dabb52032182
phillimorland <phillippamorland@gmail.com>
2021-06-02 07:47:08 +0200

    IdeasCreate//FormFields: add textinput formfield component with error handling and add to idea create form

---
+++ components/formFields.styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  formLabel: {
+    fontSize: 15,
+    lineHeight: 20,
+    paddingTop: 10,
+  },
+  textInput: {
+    borderBottomColor: '#d8d8d8',
+    borderBottomWidth: 1,
+    height: 40,
+    fontSize: 18,
+  },
+  formTitle: {
+    fontSize: 17,
+    lineHeight: 25,
+    fontWeight: 'bold'
+  },
+  formError: {
+    fontSize: 10,
+    color: 'red'
+  },
+});
