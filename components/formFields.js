0962ab12ceb10d373166a4dff024a2068fd75a31
phillimorland <phillippamorland@gmail.com>
2021-06-02 07:47:08 +0200

    IdeasCreate//FormFields: add textinput formfield component with error handling and add to idea create form

---
+++ components/formFields.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextInput, View, Text } from 'react-native';
+import { styles } from './formFields.styles';
+
+export const TextInputFormField = (props) => {
+  return (
+    <View>
+      <Text style={styles.formLabel}>{props.field}</Text>
+      <TextInput
+        style={styles.textInput}
+        {...props}
+      />
+      {props.touched && <Text style={styles.formError}>{props.error}</Text>}
+    </View>
+  );
+};
