69c99bfe6f69eb40a1b3bc0a62253f4eaaafd15f
phillimorland <phillippamorland@gmail.com>
2021-06-02 14:20:06 +0200

    formFields: add checkbox component and wrapper component

--- components/formFields.js
+++ components/formFields.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 import { styles } from './formFields.styles';
 
 export const TextInputFormField = (props) => {
@@ -11,6 +12,37 @@ export const TextInputFormField = (props) => {
         {...props}
       />
       {props.touched && <Text style={styles.formError}>{props.error}</Text>}
+    </View>
+  );
+};
+
+export const MultiInputFormField = (props) => {
+  return (
+    <View>
+      <Text style={styles.formLabel}>{props.field}</Text>
+      {props.children}
+      {props.children.touched && <Text style={styles.formError}>{props.error}</Text>}
+    </View>
+  );
+};
+
+
+export const CheckBoxFormField = (props) => {
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <View style={styles.checkBoxContainer}>
+      <CheckBox
+        checked={checked}
+        onIconPress={() => setChecked(!checked)}
+        onLongIconPress={() =>
+          console.log('onLongIconPress()')
+        }
+        onLongPress={() => console.log('onLongPress()')}
+        onPress={() => console.log('onPress()')}
+        title={props.title}
+        titleProps={{}}
+        uncheckedColor='#F00'
+      />
     </View>
   );
 };
