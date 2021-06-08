d0f93387c434dd88287fe2f3f4af66012ddc07e7
phillimorland <phillippamorland@gmail.com>
2021-06-08 17:18:59 +0200

    styles: refactor of image adding styles and add as seperate component, also update fornt sizes and add line heights inline with zeplin

--- theme/fonts.js
+++ theme/fonts.js
@@ -1,4 +1,5 @@
-const baseFontSize = 16;
+const baseFontSize = 17;
+const baseLineHeight = 22;
 
 export const FONTS = {
   familySans: '"SourceSansPro", sans-serif',
@@ -7,9 +8,19 @@ export const FONTS = {
 
 export const SIZES = {
   xs: baseFontSize * 0.8,
-  sm: baseFontSize * 0.875,
+  sm: baseFontSize * 0.85,
   base: baseFontSize,
-  md: baseFontSize * 1.125,
-  lg: baseFontSize * 1.5,
+  md: baseFontSize * 1.2,
+  lg: baseFontSize * 1.65,
+  xl: baseFontSize * 2,
   multiplyBy: times => times * baseFontSize
+};
+
+export const LINEHEIGHTS = {
+  sm: baseLineHeight * 0.95,
+  base: baseLineHeight,
+  md: baseLineHeight * 1.1,
+  lg: baseLineHeight * 1.3,
+  xl: baseLineHeight * 1.9,
+  multiplyBy: times => times * baseLineHeight
 };
