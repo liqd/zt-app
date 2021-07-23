bdc2bf816832f1a59331e8f2d20fc24dfcdbd43a
rine <k.schmid@liqd.net>
2021-07-23 13:45:59 +0200

    BaseApi: add rating functionality

--- BaseApi.js
+++ BaseApi.js
@@ -4,7 +4,8 @@ const endpoints = {
   ideas: baseUrl + '/modules/$moduleId/ideas/',
   login: baseUrl + '/login/',
   projects: baseUrl + '/app-projects/',
-  modules: baseUrl + '/app-modules/'
+  modules: baseUrl + '/app-modules/',
+  rate: baseUrl + '/contenttypes/$contentTypeId/objects/$objectPk/ratings/'
 };
 
 const makeGetRequest = (url) => {
@@ -42,6 +43,23 @@ const makePostRequest = (url, data = {}, token=null) => {
     .catch(error => console.error(error));
 };
 
+const makePutRequest = (url, data = {}, token=null) => {
+  return fetch(url, {
+    method: 'PUT',
+    headers: getHeaders(token),
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      const statusCode = response.status;
+      const data = response.json();
+      return Promise.all([statusCode, data]);
+    })
+    .then(values => {
+      return {'statusCode': values[0], 'data': values[1]};
+    })
+    .catch(error => console.error(error));
+};
+
 const API = {
   getIdeas(moduleId) {
     const url = endpoints.ideas.replace(/\$(\w+?)\b/g, moduleId);
@@ -63,6 +81,16 @@ const API = {
   getModule(moduleId) {
     const url = endpoints.modules + moduleId;
     return makeGetRequest(url);
+  },
+  rate(contentTypeId, objectPk, data, token=null) {
+    const ct_url = endpoints.rate.replace('$contentTypeId', contentTypeId);
+    const url = ct_url.replace('$objectPk', objectPk);
+    return makePostRequest(url, data, token);
+  },
+  changeRating(contentTypeId, objectPk, ratingId, data, token=null) {
+    const ct_url = endpoints.rate.replace('$contentTypeId', contentTypeId);
+    const url = ct_url.replace('$objectPk', objectPk) + ratingId +'/';
+    return makePutRequest(url, data, token);
   }
 };
 
