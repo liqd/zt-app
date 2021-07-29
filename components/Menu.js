209de15b816aa91a025167ea5f3111dbe5e67770
Kha <k.tran@liqd.net>
2021-07-29 14:04:33 +0200

    components/menu: styling to match with designs android and ios

--- components/Menu.js
+++ components/Menu.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { BottomSheet } from 'react-native-elements';
 import { MenuItem } from './MenuItem';
 
 export const Menu = (props) => {
   return (
-    <BottomSheet isVisible>
-      <View>
-        {props.menuItems.map((menuitem, idx) => <MenuItem key={idx} item={menuitem} />)}
-        <MenuItem item={{title: 'Cancel'}} />
+    <BottomSheet
+      isVisible={props.isVisible}
+      containerStyle={styles.backdrop}
+    >
+      <View style={styles.list}>
+        {props.menuItems.map((menuitem, idx) =>
+          <MenuItem key={idx} item={menuitem} />)}
       </View>
     </BottomSheet>
   );
 };
+
+const styles = StyleSheet.create({
+  backdrop: {
+    backgroundColor: 'rgba(77, 77, 77, 0.9)'
+  },
+  list: {
+    alignItems: 'center'
+  }
+});
--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import { TouchableWithoutFeedback } from 'react-native';
+import { TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import IconSLI from 'react-native-vector-icons/SimpleLineIcons';
+import { COLORS } from '../theme/colors';
+import { SIZES } from '../theme/fonts';
 
 export const MenuItem = (props) => {
+  const {item: { isCancel, isFirst, isLast }} = props;
+
   return (
     <ListItem
       Component={TouchableWithoutFeedback}
       onPress={props.item.action}
-      bottomDivider
+      bottomDivider={!isCancel}
+      containerStyle={isCancel
+        ? styles.cancelButton
+        : isFirst
+          ? styles.listButtonFirst
+          : isLast
+            ? styles.listButtonLast
+            : styles.listButton
+      }
     >
-      {props.item.icon ? <IconSLI name={props.item.icon} /> : null}
-      <ListItem.Content>
-        <ListItem.Title>{props.item.title}</ListItem.Title>
+      {props.item.icon ?
+        <IconSLI
+          name={props.item.icon}
+          color={COLORS.primary}
+          size={SIZES.md}
+        /> : null}
+      <ListItem.Content style={isCancel ? styles.cancelContent : null}>
+        <ListItem.Title
+          style={isCancel ? styles.cancelButtonText : null}
+        >
+          {props.item.title}
+        </ListItem.Title>
       </ListItem.Content>
     </ListItem>
   );
 };
+
+const styles = StyleSheet.create({
+  listButton: {
+    width: '90%',
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  listButtonFirst: {
+    width: '90%',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  listButtonLast: {
+    width: '90%',
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+  cancelContent: {
+    alignItems: 'center',
+  },
+  cancelButton: {
+    backgroundColor: 'transparent'
+  },
+  cancelButtonText: {
+    color: 'white'
+  }
+});
