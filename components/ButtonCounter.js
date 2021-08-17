5e3f2b2311b134cea4d6426ac441cb7e64c8f030
Kha <k.tran@liqd.net>
2021-08-17 15:50:16 +0200

    app/voting: highlight user vote

--- components/ButtonCounter.js
+++ components/ButtonCounter.js
@@ -9,12 +9,22 @@ export const ButtonCounter = (props) => {
       onPress={props.onPress}
       disabled={props.disabled}
     >
-      <Text style={props.disabled ? styles.textDisabled : styles.text}>
+      <Text style={
+        props.disabled
+          ? styles.textDisabled
+          : props.highlight
+            ? styles.textHighlight
+            : styles.text
+      }>
         {props.counter}
       </Text>
-      <Text
-        style={props.disabled ? styles.iconDisabled : styles.icon}
-      >
+      <Text style={
+        props.disabled
+          ? styles.iconDisabled
+          : props.highlight
+            ? styles.iconHighlight
+            : styles.icon
+      }>
         {props.icon}
       </Text>
     </TouchableOpacity>
