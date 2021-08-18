3e7d138d5e5c5ddddfe761ff6c3eb33baf37a6dd
Kha <k.tran@liqd.net>
2021-08-18 09:39:35 +0200

    app/voting: bugfix null case for vote highlighting

--- containers/Ideas/Idea.js
+++ containers/Ideas/Idea.js
@@ -201,14 +201,14 @@ export const Idea = (props) => {
               icon={<Icon name='arrow-up' size={18} />}
               counter={upVotes}
               onPress={() => handleVote('up')}
-              highlight={userRating.value === 1}
+              highlight={userRating && userRating.value === 1}
               disabled={!params.has_rating_permission}
             />
             <ButtonCounter
               icon={<Icon name='arrow-down' size={18} />}
               counter={downVotes}
               onPress={() => handleVote('down')}
-              highlight={userRating.value === -1}
+              highlight={userRating && userRating.value === -1}
               disabled={!params.has_rating_permission}
             />
           </View>
