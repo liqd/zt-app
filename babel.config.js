afa41199b76490f209fe9ecfeadce885d80a00a3
Julian Dehm <j.dehm@liqd.net>
2021-09-09 15:41:35 +0200

    add testing frameworks
    * use Jest as testing framework
    * use react testing library for DOM testing

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+      ['@babel/plugin-proposal-private-methods', {'loose': false}],
+      ['@babel/plugin-proposal-class-properties', {'loose': false}],
+      ['@babel/plugin-proposal-private-property-in-object', {'loose': false}],
+    ]
   };
 };
