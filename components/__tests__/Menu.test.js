3593160017fd9c0974b181bef709efd30a639368
Julian Dehm <j.dehm@liqd.net>
2021-09-09 15:45:01 +0200

    tests: add tests for Menu.js

---
+++ components/__tests__/Menu.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { Menu } from '../Menu';
+
+test('Test Menu isVisible false', () => {
+  const { toJSON } = render(<Menu isVisible={false} menuItems={[]} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test Menu isVisible true', () => {
+  const { toJSON } = render(<Menu isVisible={true} menuItems={[]} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test Menu with MenuItems', () => {
+  const menuItems = [
+    {isCancel: false, isFirst: false, isLast: false, isAllowed: true},
+    {isCancel: false, isFirst: false, isLast: false, isAllowed: true, icon: 'check'},
+    {isCancel: false, isFirst: false, isLast: false, isAllowed: false},
+  ];
+  const { toJSON } = render(<Menu isVisible={true} menuItems={menuItems} />);
+  expect(toJSON()).toMatchSnapshot();
+});
