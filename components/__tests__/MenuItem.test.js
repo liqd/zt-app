6aa43bc0b2b36ee77b768ca8bb78872d361f5c3e
Julian Dehm <j.dehm@liqd.net>
2021-09-09 12:26:50 +0200

    tests: add test for MenuItem.js

---
+++ components/__tests__/MenuItem.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { MenuItem } from '../MenuItem';
+
+test('Test MenuItem notAllowed', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: false, isFirst: false, isLast: false, isAllowed: false}} />);
+  expect(toJSON()).toBe(null);
+});
+
+// Snapshot tests
+test('Test MenuItem isAllowed', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: false, isFirst: false, isLast: false, isAllowed: true}} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test MenuItem isAllowed with Icon', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: false, isFirst: false, isLast: false, isAllowed: true, icon: 'check'}} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test MenuItem isAllowed isCancel', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: true, isFirst: false, isLast: false, isAllowed: true}} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test MenuItem isAllowed isFirst', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: false, isFirst: true, isLast: false, isAllowed: true}} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test MenuItem isAllowed isLast', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: false, isFirst: false, isLast: true, isAllowed: true}} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test MenuItem isAllowed isFirst and isLast', () => {
+  const { toJSON} = render(<MenuItem item={{isCancel: false, isFirst: true, isLast: true, isAllowed: true}} />);
+  expect(toJSON()).toMatchSnapshot();
+});
