90104abaf058b2df4c91813f9a33befcf066aa8c
Julian Dehm <j.dehm@liqd.net>
2021-09-16 11:41:59 +0200

    add more tests, update snapshots

---
+++ components/__tests__/ButtonSignOut.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ButtonSignOut } from '../ButtonSignOut';
+import { render } from '@testing-library/react-native';
+
+test('Test ButtonSignOut', () => {
+  const { toJSON } = render(<ButtonSignOut />);
+  expect(toJSON()).toMatchSnapshot();
+});
---
+++ components/__tests__/Modal.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { Modal } from '../Modal';
+
+test('Test Modal isVisible false', () => {
+  const { toJSON} = render(<Modal isVisible={false} modalItems={[]} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test Modal isVisible true', () => {
+  const { toJSON} = render(<Modal isVisible={true} modalItems={[]} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test Modal with ModalItems', () => {
+  const modalItems = [
+    {isCancel: false, isText: false},
+    {isCancel: false, isText: false, action: () => {}},
+    {isCancel: true, isText: false},
+    {isCancel: false, isText: true},
+    {isCancel: true, isText: true},
+  ];
+  const { toJSON} = render(<Modal isVisible={true} modalItems={modalItems} />);
+  expect(toJSON()).toMatchSnapshot();
+});
