99704070819971fa5d0301847e862c674093cbaa
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-09-21 15:28:49 +0200

    config: fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      ['@babel/plugin-proposal-private-methods', {'loose': false}],
-      ['@babel/plugin-proposal-class-properties', {'loose': false}],
-      ['@babel/plugin-proposal-private-property-in-object', {'loose': false}],
-    ]
+    presets: ['babel-preset-expo'],
   };
 };
