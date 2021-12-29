ef17212ac068f9e7487908e0be205b8c14324826
Kha <k.tran@liqd.net>
2021-12-29 15:45:33 +0000

    ideas/list: if wrong backend or no ideas fetched

--- containers/Ideas/IdeasList.js
+++ containers/Ideas/IdeasList.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 
-import { IdeasListItem } from './IdeasListItem';
+import {IdeasListItem} from './IdeasListItem';
+import {TextSourceSans} from '../../components/TextSourceSans';
 
-export const IdeasList = (props) => {
+export const IdeasList = props => {
   //do not pass ideas to child components
   const {ideas, ...otherProps} = props;
+  const hasIdeas = !!ideas && ideas.length > 0;
   return (
     <View>
-      {props.ideas.map((idea) => {
-        return (
-          <IdeasListItem
-            key={`pk${idea.pk}`}
-            idea={idea}
-            {...otherProps}
-          />
-        );
-      })}
+      {hasIdeas ? (
+        props.ideas.map(idea => {
+          return (
+            <IdeasListItem key={`pk${idea.pk}`} idea={idea} {...otherProps} />
+          );
+        })
+      ) : (
+        <TextSourceSans>
+          Could not fetch ideas. Do you use the correct Backend and is your
+          local adhocracy+ server running?
+        </TextSourceSans>
+      )}
     </View>
   );
 };
