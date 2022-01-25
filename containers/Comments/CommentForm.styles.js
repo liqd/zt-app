41a3aac52bf7d28bd162e32a33ce5778b9479142
Kha <k.tran@liqd.net>
2022-01-25 16:15:16 +0000

    ideas/commenting: Submit button sticky and focus (no styling)

---
+++ containers/Comments/CommentForm.styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { SPACINGS } from '../../theme/spacings';
+import { COLORS } from '../../theme/colors';
+
+export const styles = StyleSheet.create({
+  submitContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  textInputContainer: {
+    flex: 8
+  },
+  submitButton: {
+  }
+});
