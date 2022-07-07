bc62c71d06d323d816d0da43512501f62a326374
Julian Dehm <j.dehm@liqd.net>
2022-07-07 17:15:09 +0200

    add initial tests for comments + ideas

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,9 @@
-export default from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import AsyncStorageMock from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+AsyncStorageMock.getItem = jest.fn((key) => {
+  if (key === 'authToken') {
+    return Promise.resolve('test-authToken')
+  }
+})
+
+export default AsyncStorageMock;
