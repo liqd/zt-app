6c70a0a4fcbfe1282cafde0a67f8610f72a55ce1
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-07-12 17:41:12 +0200

    container/login: add text with links and style

---
+++ components/LinkTextSourceSans.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { TextSourceSans } from './TextSourceSans';
+
+export const LinkTextSourceSans = (props) => {
+  const mergedStyles = props.style
+    ? [styles.font, props.style]
+    : styles.font;
+  return (
+    <TextSourceSans {...props} style={mergedStyles}>
+      {props.children}
+    </TextSourceSans>
+  );
+};
+
+const styles = StyleSheet.create({
+  font: {
+    textDecorationLine: 'underline',
+  }
+});
---
+++ containers/Auth/LoginScreen.styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme/colors';
+import { SIZES, FONTWEIGHT } from '../../theme/fonts';
+import { SPACINGS } from '../../theme/spacings';
+
+export const styles = StyleSheet.create({
+  flexContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: COLORS.paper.main,
+  },
+  container: {
+    paddingHorizontal: SPACINGS.multiplyBy(.75),
+  },
+  title: {
+    marginTop: SPACINGS.multiplyBy(4),
+    marginBottom: SPACINGS.multiplyBy(0.75),
+    fontSize: SIZES.xl,
+    fontWeight: FONTWEIGHT.boldxl
+  },
+  registerText: {
+    fontSize: SIZES.base,
+    marginBottom: SPACINGS.multiplyBy(1.5)
+  },
+  forgotPassword: {
+    fontSize: SIZES.base,
+    marginTop: SPACINGS.multiplyBy(3)
+  },
+});
