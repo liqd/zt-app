e261220eb8095c43d2578614879ccb23aaf280af
Julian Dehm <j.dehm@liqd.net>
2022-07-14 16:43:44 +0200

    move header to own component

---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { styles } from './Header.styles.js';
+import { StatusBar, View } from 'react-native';
+import { Button } from '@rneui/base';
+import IconSLI from 'react-native-vector-icons/SimpleLineIcons';
+
+export const Header = (props) => {
+  const arrowLeftIcon = (<IconSLI name='arrow-left' size={22} color={props.arrowColor ? props.arrowColor : undefined} />);
+  const paddingTop = StatusBar.currentHeight || 0;
+  const backButtonStyle = props.backButtonStyle ? props.backButtonStyle : styles.backButton;
+  const backButtonTextStyle = props.backButtonTextStyle ? props.backButtonTextStyle : styles.backButtonText;
+  const containerStyle = props.transparent ? {} : styles.container;
+
+  function handleBack() {
+    if (props.isEditing !== undefined && props.isEditing){
+      props.toggleEditing();
+    }
+    else
+      return props.navigation.goBack();
+  }
+
+  const defaultLeftButton = (
+    <Button
+      buttonStyle={backButtonStyle}
+      titleStyle={backButtonTextStyle}
+      title='Back'
+      type='clear'
+      icon={arrowLeftIcon}
+      onPress={handleBack}
+    />
+  );
+
+  return (
+    <View style={{...containerStyle, paddingTop}}>
+      <View style={styles.actionsContainer}>
+        { props.leftButton ? props.leftButton : defaultLeftButton}
+        { props.rightButton }
+      </View>
+    </View>
+  );
+};
---
+++ components/__tests__/Header.test.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { fireEvent, render } from '@testing-library/react-native';
+import { Button } from '@rneui/themed';
+import IconSLI from 'react-native-vector-icons/SimpleLineIcons';
+import { Header } from '../Header';
+import { COLORS } from '../../theme/colors';
+import { SIZES } from '../../theme/fonts';
+import { SPACINGS } from '../../theme/spacings';
+import { styles } from '../../containers/Ideas/IdeaProject.styles';
+
+test('Snapshot Default Header', () => {
+  const { toJSON } = render(<Header />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Snapshot Default Header Transparent White Arrow', () => {
+  const { toJSON } = render(<Header transparent={true} arrowColor={COLORS.paper.main} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Snapshot Default Header Custom Style', () => {
+  const diffStyles = StyleSheet.create({
+    backButton: {
+      padding: 5,
+      marginVertical: 0,
+    },
+    backButtonText: {
+      fontSize: SIZES.md,
+      marginHorizontal: SPACINGS.multiplyBy(.25),
+      color: COLORS.text.main,
+    }
+  });
+  const { toJSON } = render(<Header backButtonStyle={diffStyles.backButton} backButtonTextStyle={diffStyles.backButtonText} />);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('Test Custom Header Follow Button Left', () => {
+  const plusIcon = <IconSLI name='plus' size={24} color={COLORS.paper.main} />;
+  const followButton = (
+    <Button
+      buttonStyle={styles.button}
+      titleStyle={styles.buttonText}
+      title='Follow'
+      icon={plusIcon}
+      type='clear'
+    />
+  );
+  const { getByText, queryByText } = render(<Header leftButton={followButton} />);
+  const fb = getByText(/Follow/);
+  expect(fb).toBeTruthy();
+  const backButton = queryByText(/Back/);
+  expect(backButton).toBeFalsy();
+});
+
+test('Test Custom Header Follow Button Right', () => {
+  const plusIcon = <IconSLI name='plus' size={24} color={COLORS.paper.main} />;
+  const followButton = (
+    <Button
+      buttonStyle={styles.button}
+      titleStyle={styles.buttonText}
+      title='Follow'
+      icon={plusIcon}
+      type='clear'
+    />
+  );
+  const { getByText } = render(<Header rightButton={followButton} />);
+  const fb = getByText(/Follow/);
+  expect(fb).toBeTruthy();
+  const backButton = getByText(/Back/);
+  expect(backButton).toBeTruthy();
+});
+
+test('Test Header Back Button', async () => {
+  const navigation = { goBack: jest.fn()};
+  const { findByText } = render(<Header navigation={navigation} />);
+  const backButton = await findByText('Back');
+  fireEvent.press(backButton);
+  expect(navigation.goBack).toHaveBeenCalled();
+});
+
+test('Test Header Back Button Editing', async () => {
+  const toggleEditing = jest.fn();
+  const { findByText } = render(<Header isEditing={true} toggleEditing={toggleEditing} />);
+  const backButton = await findByText('Back');
+  fireEvent.press(backButton);
+  expect(toggleEditing).toHaveBeenCalled();
+});
+
+test('Test Custom Header Follow Button Right', async () => {
+  const plusIcon = <IconSLI name='plus' size={24} color={COLORS.paper.main} />;
+  const handleFollow = jest.fn();
+  const followButton = (
+    <Button
+      buttonStyle={styles.button}
+      titleStyle={styles.buttonText}
+      title='Follow'
+      icon={plusIcon}
+      type='clear'
+      onPress={handleFollow}
+    />
+  );
+  const { findByText } = render(<Header rightButton={followButton} />);
+  const fb = await findByText(/Follow/);
+  fireEvent.press(fb);
+  expect(handleFollow).toHaveBeenCalled();
+});
