61b994732bab754d7d13aeb19e82ba65e0dbb9b9
rine <k.schmid@liqd.net>
2022-07-19 17:09:30 +0200

    Ideas/IdeaProject//Phase: add more phase information and move to own component, fixes  #310

---
+++ containers/Ideas/Phase.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import {TextSourceSans} from '../../components/TextSourceSans';
+import { styles } from './Phase.styles';
+
+export const Phase = props => {
+  const {activePhase, futurePhases, pastPhases} = props;
+  const startDate = futurePhases ? futurePhases[0].start_date : null;
+  const endDate = pastPhases ? pastPhases[pastPhases.length-1].end_date : null;
+
+  return (
+    activePhase ?  (
+      <View style={styles.phaseContainer}>
+        <TextSourceSans style={styles.phaseText}>
+          {activePhase.name + ' (active)'}
+        </TextSourceSans>
+        <TextSourceSans style={styles.phaseDate}>
+          {activePhase.start_date} – {activePhase.end_date}
+        </TextSourceSans>
+        <TextSourceSans style={styles.phaseText}>
+          {activePhase.description}
+        </TextSourceSans>
+      </View>
+    ) : (
+      <View style={styles.phaseContainer}>
+        <TextSourceSans>
+          Participation is not possible at the moment.
+        </TextSourceSans>
+        {pastPhases ? (
+          <View>
+            {futurePhases ? (
+              <TextSourceSans>
+                It continues on {startDate}.
+              </TextSourceSans>
+            ) : (
+              <TextSourceSans>
+                  It ended on {endDate}.
+              </TextSourceSans>
+            )}
+          </View>
+        ) : (
+          <View>
+            <TextSourceSans>
+              It starts on {startDate}.
+            </TextSourceSans>
+          </View>
+        )}
+      </View>
+    )
+  );
+};
---
+++ containers/Ideas/Phase.styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { SIZES, FONTWEIGHT } from '../../theme/fonts';
+import { SPACINGS } from '../../theme/spacings';
+
+export const styles = StyleSheet.create({
+  phaseContainer: {
+    marginVertical: SPACINGS.multiplyBy(1.5),
+  },
+  phaseDate: {
+    fontSize: SIZES.multiplyBy(0.9375),
+    fontWeight: FONTWEIGHT.boldxl,
+  },
+  phaseText: {
+    fontSize: SIZES.multiplyBy(0.9375),
+  }
+});
