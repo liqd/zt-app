0158d4ecc022af37e91364a64b979e4912807c6f
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-07-26 09:15:18 +0200

    User/*: ad setting overview and profile setting pages - no styling

--- containers/User/ProfileScreen.js
+++ containers/User/ProfileScreen.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { View } from 'react-native'
 import { styles } from './ProfileScreen.styles'
-
 import { ButtonSignOut } from '../../components/ButtonSignOut'
 import { Header } from '../../components/Header'
 import { TextSourceSans } from '../../components/TextSourceSans'
 import { AvatarCircle } from '../../components/ButtonAvatar'
+import IconSLI from 'react-native-vector-icons/SimpleLineIcons'
+import { Button } from '@rneui/base'
 
 export const ProfileScreen = (props) => {
+
+  const toSettingsOverview = () => {
+    props.navigation.navigate('SettingsOverview', {name: 'Bob'})
+  }
+
+  const settingsIcon = <IconSLI name='settings' size={22} />
+  const rightHeaderButton = (
+    <Button
+      icon={settingsIcon}
+      type='clear'
+      onPress={toSettingsOverview}
+    />)
+
   return (
     <View style={styles.container}>
-      <Header navigation={props.navigation} />
+      <Header
+        navigation={props.navigation}
+        rightButton={rightHeaderButton}
+      />
       <View style={styles.containerInner}>
         <AvatarCircle avatarSize={styles.avatarSize}></AvatarCircle>
         <TextSourceSans>
